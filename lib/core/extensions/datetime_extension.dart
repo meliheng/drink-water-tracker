2a20f33ef2b709fda73a7a20b22944e87dc0887b
meliheng <69640069+meliheng@users.noreply.github.com>
2025-01-31 12:32:35 +0300

    datetime extension

---
+++ lib/core/extensions/datetime_extension.dart
@@ -0,0 +1,55 @@
+extension DateTimeExtension on DateTime {
+  bool get isToday {
+    final now = DateTime.now();
+    return year == now.year && month == now.month && day == now.day;
+  }
+
+  String get formattedDate {
+    return '$day/${month.toString().padLeft(2, '0')}/$year';
+  }
+
+  String get formattedTime {
+    return '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}';
+  }
+
+  String get formattedDateTime {
+    return '$formattedDate $formattedTime';
+  }
+
+  bool isSameDay(DateTime other) {
+    return year == other.year && month == other.month && day == other.day;
+  }
+
+  int get weekNumber {
+    final firstDayOfYear = DateTime(year, 1, 1);
+    final daysIntoYear = difference(firstDayOfYear).inDays;
+    return ((daysIntoYear + firstDayOfYear.weekday - 1) / 7).ceil();
+  }
+
+  DateTime get startOfDay => DateTime(year, month, day);
+  
+  DateTime get endOfDay => DateTime(year, month, day, 23, 59, 59);
+
+  bool isBetween(DateTime start, DateTime end) {
+    return isAfter(start) && isBefore(end);
+  }
+
+  String timeAgo() {
+    final now = DateTime.now();
+    final difference = now.difference(this);
+
+    if (difference.inDays > 365) {
+      return '${(difference.inDays / 365).floor()} years ago';
+    } else if (difference.inDays > 30) {
+      return '${(difference.inDays / 30).floor()} months ago';
+    } else if (difference.inDays > 0) {
+      return '${difference.inDays} days ago';
+    } else if (difference.inHours > 0) {
+      return '${difference.inHours} hours ago';
+    } else if (difference.inMinutes > 0) {
+      return '${difference.inMinutes} minutes ago';
+    } else {
+      return 'just now';
+    }
+  }
+}
